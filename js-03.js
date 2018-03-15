23cca5424265995f7e49ec2a37388f049c02c354
ngocductaka <ngocductaka@gmail.com>
2018-03-15 23:13:36 +0700

    first commit

---
+++ js-03.js
@@ -0,0 +1,5 @@
+// 1 this & Object prototype
+// 1.1 this:
+coi ham 
+// this là một trong những cơ chế gây rối nhất trong JS, theo em this là gì ?
+// Cách hiểu 1: this trỏ tới function f, đúng hay sai?
